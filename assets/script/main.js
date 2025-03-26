f91444c16159d17da7349407c8fb44d207a4da96
dai80 <bwtrb80@yahoo.co.jp>
2025-03-26 23:46:43 +0900

    section voice

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,7 +3,26 @@ $('.voice__slick').slick({
   infinite: true,
   dots: true,
   arrows: true,
+  slidesToShow: 3,       // 2枚同時に表示
+  slidesToScroll: 3,     // 2枚ずつスクロール
   prevArrow: '<img src="/assets/image/voice_arrow-left01.png" class="slide-arrow prev-arrow">',
   nextArrow: '<img src="/assets/image/voice_arrow-right01.png" class="slide-arrow next-arrow">',
-  fade: true
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 769,  // 1024px以上で2枚表示
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }
+  ]
+
 });
+
