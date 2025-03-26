34e2f71d69360e0e4e62579016ca3f0acc11e88f
dai80 <bwtrb80@yahoo.co.jp>
2025-03-26 21:32:58 +0900

    first commit

---
+++ assets/script/main.js
@@ -0,0 +1,9 @@
+// カルーセル
+$('.voice__slick').slick({
+  infinite: true,
+  dots: true,
+  arrows: true,
+  prevArrow: '<img src="/assets/image/voice_arrow-left01.png" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="/assets/image/voice_arrow-right01.png" class="slide-arrow next-arrow">',
+  fade: true
+});
