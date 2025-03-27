6c7a414f2395a426557de743b811415f096d50fb
dai80 <bwtrb80@yahoo.co.jp>
2025-03-27 23:16:36 +0900

    footer作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,36 +1,36 @@
 // カルーセル
 $('.voice__slick').slick({
-  infinite: true,
-  dots: true,
-  arrows: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  prevArrow: '<img src="/assets/image/voice_arrow-left01.png" class="slide-arrow prev-arrow">',
-  nextArrow: '<img src="/assets/image/voice_arrow-right01.png" class="slide-arrow next-arrow">',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 769,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3
-      }
-    }
-  ]
+    infinite: true,
+    dots: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<img src="/assets/image/voice_arrow-left01.png" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="/assets/image/voice_arrow-right01.png" class="slide-arrow next-arrow">',
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        }
+    ]
 });
 
 // アコーディオン
 $('.qa-box').on('click', function (e) {
-  $(this).toggleClass('open-active')
-  var content = $(this).next();
+    $(this).toggleClass('open-active')
+    var content = $(this).next();
 
-  content.slideToggle();
+    content.slideToggle();
 });
 
 // ページトップボタン
@@ -38,11 +38,11 @@ var buttonToTop = $('#button-to-top');
 var position = 0;
 var speed = 800;
 
-buttonToTop.on('click', function() {
+buttonToTop.on('click', function () {
 
-  $("html, body").animate({
-    scrollTop: position
-  }, speed, "swing");
-  return false;
+    $("html, body").animate({
+        scrollTop: position
+    }, speed, "swing");
+    return false;
 
 });
